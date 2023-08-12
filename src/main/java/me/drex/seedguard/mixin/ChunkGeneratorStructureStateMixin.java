9407a1e198f167fecf87eb287ce3772f464dc664
Drex <nicknamedrex@gmail.com>
2023-08-12 14:44:21 +0200

    Add Stronghold salted placement

--- src/main/java/me/drex/seedguard/mixin/ChunkGeneratorStructureStateMixin.java
+++ src/main/java/me/drex/seedguard/mixin/ChunkGeneratorStructureStateMixin.java
@@ -1,38 +1,51 @@
 package me.drex.seedguard.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import me.drex.seedguard.SeedManager;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
+import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
-import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.ConcentricRingsStructurePlacement;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.List;
 
 @Mixin(ChunkGeneratorStructureState.class)
 public class ChunkGeneratorStructureStateMixin {
 
-    @ModifyArg(
+    @Inject(
         method = "createForNormal",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/chunk/ChunkGeneratorStructureState;<init>(Lnet/minecraft/world/level/levelgen/RandomState;Lnet/minecraft/world/level/biome/BiomeSource;JJLjava/util/List;)V"
-        ), index = 4
+        )
     )
-    private static List<Holder<StructureSet>> modifyFeatureSalts(List<Holder<StructureSet>> original) {
-        return original.stream().map(ChunkGeneratorStructureStateMixin::modifyFeatureSalt).toList();
+    private static void modifyFeatureSalts(RandomState randomState, long l, BiomeSource biomeSource, HolderLookup<StructureSet> holderLookup, CallbackInfoReturnable<ChunkGeneratorStructureState> cir, @Local List<Holder<StructureSet>> list) {
+        list.forEach(holder -> {
+            StructureSet structureSet = holder.value();
+            int salt = SeedManager.getStructureSeed(holder);
+            ((StructurePlacementAccessor) structureSet.placement()).setSalt(salt);
+        });
     }
 
-    private static Holder<StructureSet> modifyFeatureSalt(Holder<StructureSet> holder) {
-        StructureSet structureSet = holder.value();
-        if (structureSet.placement() instanceof RandomSpreadStructurePlacement) {
-            // ConcentricRingsStructurePlacement doesn't use the salt
-            int salt = SeedManager.getStructureSeed(holder);
-            ((StructurePlacementAccessor)structureSet.placement()).setSalt(salt);
-        }
-        return holder;
+    // Using @ModifyArgs to capture target method arguments
+    @ModifyArgs(
+        method = "generateRingPositions",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/util/RandomSource;setSeed(J)V"
+        )
+    )
+    private void modifyRingPlacementFeatureSalt(Args args, Holder<StructureSet> holder, ConcentricRingsStructurePlacement structurePlacement) {
+        args.set(0, (long) SeedManager.getStructureSeed(holder));
     }
 
 }
