d588c0e9de8b2f785779f476572e2c4e7b6f3d4b
Drex <nicknamedrex@gmail.com>
2025-09-30 20:13:44 +0200

    Update to 1.21.9

--- src/main/java/me/drex/seedguard/mixin/MinecraftServerMixin.java
+++ src/main/java/me/drex/seedguard/mixin/MinecraftServerMixin.java
@@ -2,7 +2,6 @@ package me.drex.seedguard.mixin;
 
 import me.drex.seedguard.SeedManager;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.progress.ChunkProgressListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,11 +14,11 @@ public abstract class MinecraftServerMixin {
         method = "createLevels",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/server/level/ServerLevel;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lnet/minecraft/world/level/storage/ServerLevelData;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/level/dimension/LevelStem;Lnet/minecraft/server/level/progress/ChunkProgressListener;ZJLjava/util/List;ZLnet/minecraft/world/RandomSequences;)V",
+            target = "Lnet/minecraft/server/level/ServerLevel;<init>(Lnet/minecraft/server/MinecraftServer;Ljava/util/concurrent/Executor;Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lnet/minecraft/world/level/storage/ServerLevelData;Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/world/level/dimension/LevelStem;ZJLjava/util/List;ZLnet/minecraft/world/RandomSequences;)V",
             ordinal = 0
         )
     )
-    private void loadFeatureSeeds(ChunkProgressListener chunkProgressListener, CallbackInfo ci) {
+    private void loadFeatureSeeds(CallbackInfo ci) {
         SeedManager.load((MinecraftServer) (Object) this);
     }
 
